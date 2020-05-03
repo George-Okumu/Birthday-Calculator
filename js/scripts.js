8cbb42cfb6be9722e626db18d9bf480f8a40a491
George Okumu <gokumu12@gmail.com>
2020-05-03 21:32:28 +0300

    added a meta tag in the head section

--- js/scripts.js
+++
@@ -1,5 +0,0 @@
-var daysOfTheWeek = function(number1, number2){return number1 + number2;};
-var number1 = parseInt(prompt("Enter a number"));
-var number2 = parseInt(prompt("Enter another number"));
-var result = add(number1, number2);
-prompt(result);
