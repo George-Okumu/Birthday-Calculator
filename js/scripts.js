8bc61931cdee012494f8cd07cf785de659eb9db6
George Okumu <gokumu12@gmail.com>
2020-05-07 21:13:37 +0300

    deleted the first array because it was not working

--- js/scripts.js
+++
@@ -1,3 +0,0 @@
-var maleNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
-var femaleNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
-var dayOfTheWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
